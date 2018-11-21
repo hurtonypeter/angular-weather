5e57b30d96550695b2772602de5d4e1f6347cd4a
Hurtony Péter <hurtonypeter@gmail.com>
2018-11-22 00:18:00 +0100

    starting tab view

--- src/app/core/services/cities.service.ts
+++ src/app/core/services/cities.service.ts
@@ -1,7 +1,33 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+const CITIES_KEY = 'cities';
+interface UserCities {
+  username: string;
+  cities: string[];
+}
 
 @Injectable()
 export class CitiesService {
 
-  constructor() { }
+  private _cities: BehaviorSubject<string[]>;
+
+  constructor(
+    private authService: AuthService
+  ) { 
+    this.authService.CurrentUserName.subscribe(username => {
+      this._cities.next(this.getCitiesForUser(username));
+    });
+  }
+
+  get Cities(): Observable<string[]> {
+    return this._cities.asObservable();
+  }
+
+  private getCitiesForUser(username: string): string[] {
+    const cities: UserCities[] = JSON.parse(localStorage.getItem(CITIES_KEY)) || [];
+    const userCities = cities.find(c => c.username === username);
+    return userCities ? userCities.cities : [];
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,6 +6,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [],
@@ -16,7 +17,8 @@ import { MatInputModule } from '@angular/material/input';
     MatCardModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTabsModule
   ],
   exports: [
     CommonModule,
@@ -25,7 +27,8 @@ import { MatInputModule } from '@angular/material/input';
     MatCardModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTabsModule
   ]
 })
 export class SharedModule { }
